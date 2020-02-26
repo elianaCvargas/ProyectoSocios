2170c548abd7bf1a40467b1040d063fd2364b00b
Rodrigo Vallaro <rodrigo.vallaro@resolvit.com>
2020-02-25 23:01:13 -0300

    Agrego Archivo socios

--- Challenge_SuperLiga/Scripts/Home-index.js
+++ Challenge_SuperLiga/Scripts/Home-index.js
@@ -1,8 +1,6 @@
 ﻿
 $(document).ready(function () {
-	//$("#btnImportar").click(
-	//	mostrarNav()
-	//);
+
 });
 
 function procesarArchivo() {
@@ -13,3 +11,14 @@ function procesarArchivo() {
 function mostrarNav() {
 	$(".isFilled").toggleClass("hayDatosClass");
 }
+
+function VerificarSpan() {
+	var nameFile = $("#file-title").text();
+	if (nameFile != null || nameFile != undefined || nameFile != "") {
+		$("#btnImportar").prop('disabled', true);
+	}
+	else {
+		$("#btnImportar").prop('disabled', false);
+	}
+}
+
